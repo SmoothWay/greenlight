03a0fad4774e4fd13bca3ded311f33d919bbedd3
SmoothWay <erakarbay@gmail.com>
2022-08-20 20:53:08 +0600

    added new route to get all movies

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
+	"github.com/SmoothWay/greenlight/internal/validator"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -80,4 +82,33 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	return s
+}
+
+func (app *application) readCSV(qs url.Values, key string, defautlValue []string) []string {
+	csv := qs.Get(key)
+	if csv == "" {
+		return defautlValue
+	}
+	return strings.Split(csv, ",")
+}
+
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+	return i
 }
