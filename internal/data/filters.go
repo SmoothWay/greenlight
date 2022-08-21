6bc2000335f8dfaf0e774dc4ae2167d4f0cb0091
SmoothWay <erakarbay@gmail.com>
2022-08-21 18:02:18 +0600

    added movies pagination with url values page and page_size

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/SmoothWay/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/SmoothWay/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -9,6 +13,29 @@ type Filters struct {
 	SortSafelist []string
 }
 
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
+}
 func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.Page > 0, "page", "must be greater than zero")
 	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
