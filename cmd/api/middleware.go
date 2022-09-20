6f2b50475f286385fa80af99fbffddafc0fa12a6
SmoothWay <erakarbay@gmail.com>
2022-09-20 11:43:47 +0600

    rate limite

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+
+	"golang.org/x/time/rate"
 )
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		if !clients[ip].Allow() {
+			mu.Unlock()
+			app.ratelimitExceedResponce(w, r)
+			return
+		}
+		mu.Unlock()
+		next.ServeHTTP(w, r)
+	})
+}
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
