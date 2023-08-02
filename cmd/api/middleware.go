6d6fd97437769dc3a1f3563c2b47e164fd35edb9
SmoothWay <erakarbay@gmail.com>
2023-08-02 20:02:52 +0600

    authenticate context

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -69,3 +69,9 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+	})
+}
